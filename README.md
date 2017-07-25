f0145cb2fba13cc850113e93089c83e32ee00452
Mehdi <mehdi@lajavaness.com>
2017-07-25 22:34:19 +0200

    add sigmate

---
+++ README.md
@@ -0,0 +1,13 @@
+## you don't need a local node, just assure you there's enough kovan on the account
+
+# to migrate    
+`truffle migrate --network kovan --reset`   
+stop after 'saving artifacts'
+
+# to run test    
+`truffle test`   
+
+# to upgrade npm packages
+`cd build`   
+`npm version patch`   
+`npm publish`    
--- truffle.js
+++ truffle.js
@@ -1,3 +1,7 @@
+const LightWalletProvider = require('@digix/truffle-lightwallet-provider');
+const pass = 'Dether76';
+// kovan 0xD2805712d2fF5EFF50F8E28506F8e5A2acdD80a8
+
 module.exports = {
   networks: {
     development: {
@@ -5,29 +9,27 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     },
-    // ropsten: {
-    //   provider: new LightWalletProvider({
-    //     keystore: '/Users/ljn/.sigmate/sigmate-v3-ropstenDeploy.json',
-    //     password: 'Dether76',
-    //     rpcUrl: 'https://ropsten.infura.io/v604Wu8pXGoPC41ARh0B',
-    //     debug: true, // optional, show JSON-RPC logs
-    //     prefund: 1e18, // optional, fund all lightwallet addresses (via coinbase) with this  of wei
-    //     pollingInterval: 4000 // optional, polling interval for the provider (reduce for faster deploy with testRPC or kovan)
-    //   }),
-    //   network_id: '3',
-    // },
-    ropsten: {
-      host: "localhost",
-      port: 8545,
-      network_id: 3,
-      from: "0xe070b23860FA281252aC4ABB8d3E120f088d1Fb1",
-    },
     kovan: {
-      host: "localhost",
-      port: 8545,
+      provider: new LightWalletProvider({
+        keystore: './sigmate-v3-ropstenDeploy.json',
+        password: pass,
+        rpcUrl: 'https://kovan.infura.io/v604Wu8pXGoPC41ARh0B',
+        // debug: true, // optional, show JSON-RPC logs
+      }),
       network_id: 42,
-      from: "0xbb5a8A90F2404f7F2DBd9777168F90A746386dBa",
     },
+    // ropsten: {
+    //   host: "localhost",
+    //   port: 8545,
+    //   network_id: 3,
+    //   from: "0xe070b23860FA281252aC4ABB8d3E120f088d1Fb1",
+    // },
+    // kovan: {
+    //   host: "localhost",
+    //   port: 8545,
+    //   network_id: 42,
+    //   from: "0xbb5a8A90F2404f7F2DBd9777168F90A746386dBa",
+    // },
     mainnet: {
       host: 'localhost',
       port: 8545,
