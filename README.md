f208f0531e4c15358f6a6482b2fd205b0a69dda7
Mehdi <mehdi@lajavaness.com>
2017-07-26 18:36:52 +0200

    modif type

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
-## you don't need a local node, just assure you there's enough kovan on the account
+# you don't need a local node, just assure you there's enough kovan on the account
 
-# to migrate    
+## to migrate    
 `truffle migrate --network kovan --reset`   
 stop after 'saving artifacts'
 
-# to run test    
+## to run test    
 `truffle test`   
 
-# to upgrade npm packages    
+## to upgrade npm packages    
 https://www.npmjs.com/package/dethercontract    
 
 `cd build`   
 `npm version patch`   
 `npm publish`    
+
+
+# TO DO    
+Improve contract: when a teller register in a new zone, find a way to delete him from his last zone.
--- contracts/Dether.sol
+++ contracts/Dether.sol
@@ -3,96 +3,104 @@ pragma solidity ^0.4.11;
 import './import/Ownable.sol';
 import './import/SafeMath.sol';
 
+
 contract Dether is Ownable, SafeMath {
   // using SafeMath for uint;
-  struct Pos {
-    uint lat;
-    uint lng;
-  }
-  enum Currency {
-      USD,
-      EUR,
-      KRW,
-      CNY
-  }
-  // add event
+
   struct Teller {
     int8 rates;               // commission rates teller is going to take 3 digit with 1 decimal
     uint balance;             // balance credited in escrow
     uint volumeTrade;         // volume realised
     uint nbTrade;             // number of trade realised
-    uint lat;
-    uint lng;
-    uint zone;
+    uint  lat;
+    uint  lng;
+    uint zoneId;
     string name;
-    Currency currency;            // 1 = dollar , 2 = eur, 3 = CNY, 4 = KRW
-    int8 avatar;              // avatar
-    string telAddr;           // telegram account
+    int8 currencyId;            // 1 = dollar , 2 = eur, 3 = CNY, 4 = KRW
+    int8 avatarId;              // avatar
+    string messengerAddr;           // telegram account
   }
 
+
   address[] public listAdressesTellers;
-  address public rewardFund;
-  mapping(address => Teller) public tellers;
+  mapping(address => Teller) tellers;
 
   mapping (uint => address[]) public tellerPerZone;
-
   event Transfer (address indexed _from, address indexed _to, uint256 _value);
-  event Debug(string str, uint number);
 
-    // should take an address and put it on a zone
-  function registerPoint(uint lat, uint lng, uint zone, int8 rates, int8 avatar, Currency currency, string _address, string _name) payable {
+  function registerPoint(uint lat, uint lng, uint zone, int8 rates, int8 avatar, int8 currency, string _address, string _name) payable {
       require(msg.value > 1 finney);
+      require(msg.value + tellers[msg.sender].balance < 10 ether);
       tellers[msg.sender].rates = rates;
-      tellers[msg.sender].currency = currency;
-      tellers[msg.sender].avatar = avatar;
+      tellers[msg.sender].currencyId = currency;
+      tellers[msg.sender].avatarId = avatar;
       tellers[msg.sender].balance = add(tellers[msg.sender].balance,msg.value);
       tellers[msg.sender].lat = lat;
       tellers[msg.sender].lng = lng;
-      tellers[msg.sender].zone = zone;
       tellers[msg.sender].name = _name;
-      tellers[msg.sender].telAddr = _address;
+      tellers[msg.sender].messengerAddr = _address;
       tellerPerZone[zone].push(msg.sender);
+      tellers[msg.sender].zoneId = zone;
+  }
+
+  function getTellerPos( address _teller ) constant returns (
+    uint lat,
+    uint lng,
+    uint zone) {
+        return (tellers[_teller].lat
+        , tellers[_teller].lng
+        , tellers[_teller].zoneId);
+  }
+
+  function getTellerProfile(address _teller) constant returns (int8 rates,
+    uint volumeTrade,
+    uint nbTrade,
+    string name,
+    int8 currency,
+    int8 avatar,
+    string telAddr) {
+        return (tellers[_teller].rates
+        , tellers[_teller].volumeTrade
+        , tellers[_teller].nbTrade
+        , tellers[_teller].name
+        , tellers[_teller].currencyId
+        , tellers[_teller].avatarId
+        , tellers[_teller].messengerAddr);
   }
 
   function updatePoint(uint lat, uint lng, uint zone, int8 rates, string _address) {
-      require(tellers[msg.sender].balance > 0);
       tellers[msg.sender].rates = rates;
       tellers[msg.sender].lat = lat;
       tellers[msg.sender].lng = lng;
-      tellers[msg.sender].zone = zone;
-      tellers[msg.sender].telAddr = _address;
+      tellers[msg.sender].zoneId = zone;
+      tellers[msg.sender].messengerAddr = _address;
   }
 
-  function updatePointWithFund(uint lat, uint lng, uint zone, int8 rates, string _address) payable {
-      require(msg.value > 10 finney);
-      tellers[msg.sender].rates = rates;
+  function updatePointAddFund(uint lat, uint lng, uint zone, int8 rates, string _address) payable {
+      require(tellers[msg.sender].balance > 1 finney);
       tellers[msg.sender].balance = add(tellers[msg.sender].balance,msg.value);
+      tellers[msg.sender].rates = rates;
       tellers[msg.sender].lat = lat;
       tellers[msg.sender].lng = lng;
-      tellers[msg.sender].zone = zone;
-      tellers[msg.sender].telAddr = _address;
+      tellers[msg.sender].zoneId = zone;
+      tellers[msg.sender].messengerAddr = _address;
   }
 
-    /*
-     * Simple sendcoin with proof of burn (no commission)
-     */
   function sendCoin (address receiver, uint amount) returns (bool) {
     require(tellers[msg.sender].balance > amount);
-    uint toSendReceiver = sub(amount,(amount * 5/1000));
-    uint toSendFund = sub(amount,toSendReceiver);
-    receiver.transfer(toSendReceiver);
-    rewardFund.transfer(toSendFund);
+    receiver.transfer(amount);
     tellers[msg.sender].balance = sub(tellers[msg.sender].balance, amount);
     tellers[msg.sender].volumeTrade = add(tellers[msg.sender].volumeTrade,amount);
     ++tellers[msg.sender].nbTrade;
-    Transfer(msg.sender, receiver, toSendReceiver);
-    Transfer(msg.sender, rewardFund, toSendFund);
+    Transfer(msg.sender, receiver, amount);
     return true;
   }
+
 
   function getZone(uint zone) constant returns (address[]) {
       return tellerPerZone[zone];
   }
+
   /// withdraw the total bablance
   function withdrawAll() {
     msg.sender.transfer(tellers[msg.sender].balance);
@@ -102,4 +110,5 @@ contract Dether is Ownable, SafeMath {
   function getTellerBalances(address _address) constant returns (uint) {
     return tellers[_address].balance;
   }
+
 }
