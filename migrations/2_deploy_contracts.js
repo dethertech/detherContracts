0afdf28d85946cbfbbb49873b8b6fa93507ededa
Janaka-Steph <rochestephane@yahoo.fr>
2017-09-11 09:51:43 +0200

    Add log to know when migrations are run

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,6 +2,7 @@ var DetherStorage = artifacts.require("./DetherStorage.sol");
 var DetherInterface = artifacts.require("./DetherInterface.sol");
 
 module.exports = function(deployer) {
+  console.log('Migration --')
   deployer
     .deploy(DetherStorage, {gas: 1500000})
     .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
