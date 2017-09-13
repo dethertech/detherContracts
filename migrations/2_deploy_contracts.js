02c0ebb40a0e5827e0b29fe5f150aeee51e7777d
Mehdi Amari <mehdi.amari@gmail.com>
2017-09-13 17:50:41 +0200

    add gas

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,7 +6,7 @@ module.exports = function(deployer, network) {
   console.log('Migration --')
   if (network == 42) {
     // to deploy interface with old storage
-    deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address);
+    deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address, {gas: 1500000});
   } else {
     // todeploy interface with new storage
       deployer.deploy(DetherStorage, {gas: 1500000})
