1c316f97955113ca4270c07299a844d7f577f284
Mehdi Amari <mehdi.amari@gmail.com>
2017-09-13 22:29:45 +0200

    remove useless variable

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -6,21 +6,10 @@ module.exports = function(deployer, network) {
   console.log('Migration --', network);
   if (network == 'kovan') {
       console.log('-> kovan');
-      let storagecontract;
-      let interfacecontract;
       deployer.deploy(DetherStorage, {gas: 1500000})
       .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
-      .then(() => {
-        return DetherStorage.at(DetherStorage.address);
-      }).then((instance) => {
-        storagecontract = instance;
-        return DetherInterface.at(DetherInterface.address);
-      }).then((instance) => {
-        interfacecontract = instance;
-        return storagecontract.transferOwnership(DetherInterface.address, {gas: 300000} )
-      }).then(() => {
-        return interfacecontract.setInit({gas: 300000} );
-      })
+      .then(() => DetherStorage.at(DetherStorage.address).transferOwnership(DetherInterface.address, {gas: 300000} ))
+      .then(() => DetherInterface.at(DetherInterface.address).setInit({gas: 300000}))
     } else {
     console.log('=>Other')
       deployer.deploy(DetherStorage, {gas: 1500000})
