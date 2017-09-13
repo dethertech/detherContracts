fe7a89368c9db50cda50542bbbf37637bd2c9444
Mehdi Amari <mehdi@MacBook-Pro-de-Mehdi.local>
2017-09-13 16:03:29 +0200

    uncomment deoployer

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,11 +2,14 @@ var DetherStorage = artifacts.require("./DetherStorage.sol");
 var DetherInterface = artifacts.require("./DetherInterface.sol");
 var DetherStorageJson = require("../build/contracts/DetherStorage.json");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
   console.log('Migration --')
-  // to deploy interface with old storage
-  deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address);
-  // to deploy interface with new storage
-  //   .deploy(DetherStorage, {gas: 1500000})
-  //   .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
+  if (network == 42) {
+    // to deploy interface with old storage
+    deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address);
+  } else {
+    // todeploy interface with new storage
+      deployer.deploy(DetherStorage, {gas: 1500000})
+      .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
+  }
 };
