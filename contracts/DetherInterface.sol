5fac62bb4b4bfbd8ed3441a96f657100a769536d
Mehdi Amari <mehdi@MacBook-Pro-de-Mehdi.local>
2017-09-13 22:11:56 +0200

    add script migration and change require in register sell point

--- contracts/DetherInterface.sol
+++ contracts/DetherInterface.sol
@@ -45,7 +45,9 @@ contract DetherInterface is Ownable {
     bytes32 _name
     ) payable isNotLockedForInitialMigration {
       // Conditions
-      require(!detherStorage.isTeller(msg.sender));
+      // require(!detherStorage.isTeller(msg.sender));
+      uint balance = detherStorage.getTellerBalance(msg.sender);
+      require(balance == 0);
       require(msg.value >= 10 finney);
       require(msg.value < 5 ether);
       //
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,12 +5,23 @@ var DetherStorageJson = require("../build/contracts/DetherStorage.json");
 module.exports = function(deployer, network) {
   console.log('Migration --', network);
   if (network == 'kovan') {
-    console.log('-> kovan');
-    //deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address, {gas: 1500000});
-    deployer.deploy(DetherStorage, {gas: 1500000})
-    .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
-  } else {
-    // to deploy interface with new storage
+      console.log('-> kovan');
+      let storagecontract;
+      let interfacecontract;
+      deployer.deploy(DetherStorage, {gas: 1500000})
+      .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
+      .then(() => {
+        return DetherStorage.at(DetherStorage.address);
+      }).then((instance) => {
+        storagecontract = instance;
+        return DetherInterface.at(DetherInterface.address);
+      }).then((instance) => {
+        interfacecontract = instance;
+        return storagecontract.transferOwnership(DetherInterface.address, {gas: 300000} )
+      }).then(() => {
+        return interfacecontract.setInit({gas: 300000} );
+      })
+    } else {
     console.log('=>Other')
       deployer.deploy(DetherStorage, {gas: 1500000})
       .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
