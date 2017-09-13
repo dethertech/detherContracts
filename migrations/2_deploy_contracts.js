a1c6b223637319358ee9e16512bedf718a4866a1
Mehdi Amari <mehdi@MacBook-Pro-de-Mehdi.local>
2017-09-13 19:56:43 +0200

    up contract

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,9 +5,10 @@ var DetherStorageJson = require("../build/contracts/DetherStorage.json");
 module.exports = function(deployer, network) {
   console.log('Migration --', network);
   if (network == 'kovan') {
-    // to deploy interface with old storage
     console.log('-> kovan');
-    deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address, {gas: 1500000});
+    //deployer.deploy(DetherInterface, DetherStorageJson.networks[42].address, {gas: 1500000});
+    deployer.deploy(DetherStorage, {gas: 1500000})
+    .then(() => deployer.deploy(DetherInterface, DetherStorage.address, {gas: 1500000}))
   } else {
     // to deploy interface with new storage
     console.log('=>Other')
