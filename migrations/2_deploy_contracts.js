a0af69d22edf8f2fed8baffd553ddbcfad7ab995
rmi7 <user@rmi7.me>
2018-04-11 10:36:30 +0200

    use async awai in migration

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,15 +5,14 @@
 // var DthRegistry = artifacts.require("./DthRegistry.sol");
 // var DthShop = artifacts.require("./DetherShopStorage.sol");
 
-var DetherCore = artifacts.require("./DetherCore.sol");
-var DetherToken = artifacts.require("./dth/DetherToken.sol");
-var SmsCertifier = artifacts.require("./certifier/SmsCertifier.sol");
-var DetherBank = artifacts.require("./DetherBank.sol");
+const DetherCore = artifacts.require("./DetherCore.sol");
+const DetherToken = artifacts.require("./dth/DetherToken.sol");
+const SmsCertifier = artifacts.require("./certifier/SmsCertifier.sol");
+const DetherBank = artifacts.require("./DetherBank.sol");
 
-module.exports = function(deployer, network) {
-
-  deployer.deploy(DetherCore, {gas: 4700000, gasPrice:25000000000})
-  // .then(() => deployer.deploy(DetherToken, {gas: 4700000, gasPrice:25000000000}))
-  .then(() => deployer.deploy(DetherBank, {gas: 4700000, gasPrice:25000000000}))
-  // .then(() => deployer.deploy(SmsCertifier, {gas: 4700000, gasPrice:25000000000}))
+module.exports = async (deployer, network) => {
+  await deployer.deploy(DetherCore, {gas: 4700000, gasPrice:25000000000});
+  await deployer.deploy(DetherToken, {gas: 4700000, gasPrice:25000000000});
+  await deployer.deploy(DetherBank, {gas: 4700000, gasPrice:25000000000});
+  await deployer.deploy(SmsCertifier, {gas: 4700000, gasPrice:25000000000});
 };
