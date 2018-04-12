617dfbdd8de878fa796655794ed59a10dc7e15c5
rmi7 <user@rmi7.me>
2018-04-12 23:50:19 +0200

    update migrations to deploy ExchangeRateOracle separately

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,18 +1,14 @@
-// var DetherInterface = artifacts.require("./DetherInterface.sol");
-// var DetherStorage = artifacts.require("./DetherTellerStorage.sol");
-// var SmsCertifier = artifacts.require("./certifier/SmsCertifier.sol");
-// var DetherToken = artifacts.require("./dth/DetherToken.sol");
-// var DthRegistry = artifacts.require("./DthRegistry.sol");
-// var DthShop = artifacts.require("./DetherShopStorage.sol");
-
-const DetherCore = artifacts.require("./DetherCore.sol");
-const DetherToken = artifacts.require("./dth/DetherToken.sol");
-const SmsCertifier = artifacts.require("./certifier/SmsCertifier.sol");
-const DetherBank = artifacts.require("./DetherBank.sol");
+/* global artifacts */
+const DetherCore = artifacts.require('./DetherCore.sol');
+const DetherToken = artifacts.require('./dth/DetherToken.sol');
+const SmsCertifier = artifacts.require('./certifier/SmsCertifier.sol');
+const DetherBank = artifacts.require('./DetherBank.sol');
+const ExchangeRateOracle = artifacts.require('./ExchangeRateOracle.sol');
 
 module.exports = async (deployer, network) => {
-  await deployer.deploy(DetherCore, {gas: 4700000, gasPrice:25000000000});
-  await deployer.deploy(DetherToken, {gas: 4700000, gasPrice:25000000000});
-  await deployer.deploy(DetherBank, {gas: 4700000, gasPrice:25000000000});
-  await deployer.deploy(SmsCertifier, {gas: 4700000, gasPrice:25000000000});
+  await deployer.deploy(DetherCore, { gas: 4700000, gasPrice: 25000000000 });
+  await deployer.deploy(DetherToken, { gas: 4700000, gasPrice: 25000000000 });
+  await deployer.deploy(DetherBank, { gas: 4700000, gasPrice: 25000000000 });
+  await deployer.deploy(SmsCertifier, { gas: 4700000, gasPrice: 25000000000 });
+  await deployer.deploy(ExchangeRateOracle, 0, { gas: 4700000, gasPrice: 25000000000 });
 };
